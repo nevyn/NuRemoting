b99283ed09b89df78fd62e8e5393ac592a61a63b
Joachim Bengtsson <nevyn@spotify.com>
2011-04-13 14:01:36 +0200

    Be able to receive log msgs

--- ClientController.m
+++ ClientController.m
@@ -94,6 +94,26 @@ static NSColor *DarkGreen() {
 }
 -(void)remotingClient:(RemotingClient*)client receivedOutput:(NSString*)str withStatusCode:(int)code;
 {
+	if(code >= 600 && code < 700) {
+		int level = code-600;
+		
+		NSColor *color = nil;
+		if(level < 3) // error, assert, fatal
+			color = [NSColor redColor];
+		else if(level == 3) // warning
+			color = [NSColor colorWithCalibratedHue:.765 saturation:.7 brightness:1 alpha:1];
+		else if(level == 4) // info
+			color = [NSColor colorWithCalibratedHue:.68 saturation:.74 brightness:.8 alpha:1];
+		else if(level == 5) // spam
+			color = [NSColor brownColor];
+		else if(level == 6) // debug0
+			color = [NSColor colorWithCalibratedHue:.3 saturation:.5 brightness:.7 alpha:1];
+		else if(level > 6) // debug1-7
+			color = [NSColor colorWithCalibratedHue:.3 saturation:.5-((level-6)/7./2.) brightness:.7 alpha:1];
+
+		[self appendString:str color:color italic:YES];
+		return;
+	}
 	[self appendString:str color:(code!=RemotingStatusOK)?[NSColor redColor]:[NSColor blackColor] italic:NO];
 
 }
@@ -110,7 +130,7 @@ static NSColor *DarkGreen() {
 		NSString *templateName = [outputString substringWithRange:NSMakeRange(r.location+r.length, toNewline.location-r.location-r.length)];
 		[outputString replaceCharactersInRange:NSMakeRange(r.location, toNewline.location-r.location) withString:[templates contentsOfSnippetNamed:templateName]];
 	}
-	[self appendString:[NSString stringWithFormat:@"> %@", outputString] color:[NSColor grayColor] italic:YES];
+	[self appendString:outputString color:[NSColor purpleColor] italic:YES];
 	[client sendCommand:outputString];
 }
 @end
